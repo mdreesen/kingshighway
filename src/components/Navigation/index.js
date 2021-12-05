617a07e867e2213e3cb84cce8f423f026b1f548c
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-05 18:08:08 -0500

    splitting up how navbar component works

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,9 +7,8 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
+// import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
 // Importing custom CSS
@@ -35,114 +34,144 @@ const settings = ['Profile', 'Account', 'Dashboard'];
 export default function Navigation() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
+
     const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
+        setAnchorElNav(event.currentTarget);
     };
     const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
+        setAnchorElUser(event.currentTarget);
     };
-  
+
     const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
+        setAnchorElNav(null);
     };
-  
+
     const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
+        setAnchorElUser(null);
     };
-  
-    return (
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
+
+    const Title = () => {
+        return (
             <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
             >
-              Kings Highway
+                Kings Highway
             </Typography>
-  
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
+        );
+    }
+
+    const SubMenu = () => {
+        return (
+            <Box sx={{ flexGrow: 0 }}>
+            {/* <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
-              <Menu
+            </Tooltip> */}
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {pages.subs.map((sub) => (
+                <MenuItem key={sub} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{sub}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        );
+    }
+
+    const MobileLinkCollection = () => {
+        return (
+            <Menu
                 id="menu-appbar"
                 anchorEl={anchorElNav}
                 anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
+                    vertical: 'bottom',
+                    horizontal: 'left',
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
+                    vertical: 'top',
+                    horizontal: 'left',
                 }}
                 open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
                 sx={{
-                  display: { xs: 'block', md: 'none' },
+                    display: { xs: 'block', md: 'none' },
                 }}
-              >
+            >
                 {pages?.map((page) => (
-                  <MenuItem key={page.link} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page?.link}</Typography>
-                  </MenuItem>
+                    <MenuItem key={page.link} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{page?.link}</Typography>
+                    </MenuItem>
                 ))}
-              </Menu>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page?.link}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
+            </Menu>
+        );
+    }
+
+    const MobileMenu = () => {
+        return (
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                    size="large"
+                    aria-label="Mobile Hamburger Menu"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
                 >
-                  {page?.link}
-                </Button>
-              ))}
-            </Box>
-  
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    <MenuIcon />
                 </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+                <MobileLinkCollection />
             </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+        );
+    };
+
+    const DesktopMenu = () => {
+        return (
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                    <Button
+                        key={page?.link}
+                        onClick={handleCloseNavMenu}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                        {page?.link}
+                    </Button>
+                ))}
+            </Box>
+        );
+    }
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    {/* Title Component */}
+                    <Title />
+                    {/* Mobile Menu Component */}
+                    <MobileMenu />
+                    {/* Desktop Menu Component */}
+                    <DesktopMenu />
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
