529c9b507c9b8baeda78851d40f8db5badbc7a38
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-14 22:23:29 -0500

    changed and added bootstrap navbar. This will be a bigger change going forward

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,11 +1,4 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-// import CardContent from '@mui/material/CardContent';
-// import CardActions from '@mui/material/CardActions';
-// import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 
 import data from '../../utils/data.json';
 
@@ -15,36 +8,19 @@ export default function LeaderCard() {
     console.log(leaderdata)
 
     return (
-        <Grid container spacing={{ xs: 3, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
+        <div>
             {
                 leaderdata?.map((leader, index) => {
                     return (
-                        <Grid item xs={2} sm={3} md={4} key={index}>
-                            <Card sx={{ maxWidth: 345 }} key={`${leader.name}-${index}`}>
-                                <CardHeader
-                                    title={leader.name}
-                                />
-                                <div>
-                                    <CardMedia
-                                        component="img"
-                                        height="270"
-                                        image={`./images/${leader.image}.webp`}
-                                        alt={`leader ${leader.name}`}
-                                    />
-                                </div>
-                                {/* <CardContent>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {leader.about}
-                                    </Typography>
-                                </CardContent> */}
-                                {/* <CardActions disableSpacing>
-                                </CardActions> */}
-                            </Card>
-                        </Grid>
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
                     );
                 })
             }
-        </Grid>
-
+        </div>
     );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,15 +1,9 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Importing custom CSS
 import './nav.css';
@@ -48,175 +42,36 @@ const pages = [
         "link": "/give"
     },
 ]
-const settings = ['Profile', 'Account', 'Dashboard'];
 
 export default function Navigation() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
-    // Title Component
-    const Title = () => {
+    const Links = () => {
         return (
-            <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, justifyContent: 'center', alignItems: 'center' }}
-            >
-                KHDCI
-            </Typography>
-        );
-    }
-
-    // Mobile Title Component
-    const MobileTitle = () => {
-        return (
-            <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'center', alignItems: 'center' }}
-            >
-                KHDCI
-            </Typography>
-        );
-    }
-
-    const AboutMenu = () => {
-        return (
-            <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-            >
-                {pages?.sub?.map((sub) => (
-                    <MenuItem key={sub?.name} onClick={handleCloseNavMenu}>
-                        <Typography textAlign="center">{sub?.name}</Typography>
-                    </MenuItem>
-                ))}
-            </Menu>
-        );
-    }
-
-    // Mobile Menu Component
-    const MobileLinkCollection = () => {
-        return (
-            <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                    display: { xs: 'block', md: 'none' },
-                }}
-            >
-                {pages?.map((page) => (
-                    <MenuItem key={page?.name} onClick={handleCloseNavMenu}>
-                        <Typography textAlign="center">{page?.name}</Typography>
-                    </MenuItem>
-                ))}
-            </Menu>
-        );
-    }
-
-    // Mobile Menu Component
-    const MobileMenu = () => {
-        return (
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                    size="large"
-                    aria-label="Mobile Hamburger Menu"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit"
-                >
-                    <MenuIcon />
-                </IconButton>
-                <MobileLinkCollection />
-                {/* Mobile Title Component */}
-                <MobileTitle />
-            </Box>
-        );
-    };
-
-    console.log("about", pages?.map((page) => page?.sub?.map((sub) => sub.name)))
-
-    // Desktop Menu Component
-    const DesktopMenu = () => {
-        return (
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {/* Desktop Title Component */}
-                <Title />
-                {pages.map((page) => (
+            <Nav className="me-auto">
+                {pages.map((page) =>
                     page.sub ? (
-                        <Button
-                            key={page?.name}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            onClick={AboutMenu}
-                        >
-                            {page?.name}
-                        </Button>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            {page.sub.map((sub) => <NavDropdown.Item href={sub.link}>{sub.name}</NavDropdown.Item>)}
+                        </NavDropdown>
                     ) : (
-                        <Button
-                            key={page?.name}
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            href={page?.link}
-                        >
-                            {page?.name}
-                        </Button>
+                        <Nav.Link href={page.link}>{page.name}</Nav.Link>
                     )
-                ))}
-            </Box>
+                )}
+            </Nav>
         );
     }
 
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    {/* Mobile Menu Component */}
-                    <MobileMenu />
-                    {/* Desktop Menu Component */}
-                    <DesktopMenu />
-                </Toolbar>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">KHDCI</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        <Links />
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
-        </AppBar>
+        </Navbar>
     );
 }
