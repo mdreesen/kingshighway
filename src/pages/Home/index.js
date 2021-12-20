d6b54824909ab96d6c91028b91a34fd5abd2359f
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-20 00:07:33 -0500

    looking into making website hero like compared website

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,12 +18,16 @@ const Home = () => {
                 >
                     <source src={videoOne} />
                 </video>
+                {/* <div>
+                    <h2>Service Times</h2>
+                    <h3>10am-12pm Central Time</h3>
+                </div> */}
             </section>
             <section className="mission_content">
                 <div className="text_container">
-                    <h2>
+                    {/* <h2>
                         Our mission is to seek God’s face, teach the good news to the lost and to make disciples for the Kingdom of God, our hope is to be a blessing to you, your family and surrounding communities, we are dedicated to the gospel and continuing the works of our Lord and Savior, Jesus Christ.
-                    </h2>
+                    </h2> */}
                 </div>
                 {/* <img alt="" className="bishop_rose_img" src="./images/homePic.webp"></img> */}
             </section>
