959c4310ebfdd681d664c0deec775d276d668519
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-22 21:10:01 -0500

    adding the background color of the footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -29,6 +29,7 @@ const Home = () => {
                         Our mission is to seek God’s face, teach the good news to the lost and to make disciples for the Kingdom of God, our hope is to be a blessing to you, your family and surrounding communities, we are dedicated to the gospel and continuing the works of our Lord and Savior, Jesus Christ.
                     </h2> */}
                     <h2>Events</h2>
+                    
                 </div>
             </section>
         </div>
