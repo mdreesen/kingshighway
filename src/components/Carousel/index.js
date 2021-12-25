3ac9142dd8e2294b5a8d144f835fffaeab0f7124
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-25 15:48:00 -0500

    adding in the carousel pictures

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 // Import Images
-import imageOne from '../../utils/eventImages/eventOne.webp'
+import imageOne from '../../utils/eventImages/eventOne.webp';
 
 // Import Event Data
 import eventData from '../../utils/eventdata.json';
@@ -11,34 +11,16 @@ const Slideshow = () => {
 
     return (
         <Carousel>
-            {
-                eventData?.events?.map((item, index) => {
-                    console.log(item)
-                    return (
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={`../../utils/eventImages/${item?.imageName}.webp`}
-                                alt="First slide"
-                            />
-                            <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    );
-                })
-            }
-            {/* <Carousel.Item>
+            <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src={imageOne}
                     alt="First slide"
                 />
-                <Carousel.Caption>
+                {/* <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                </Carousel.Caption> */}
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -46,11 +28,10 @@ const Slideshow = () => {
                     src={imageOne}
                     alt="Second slide"
                 />
-
-                <Carousel.Caption>
+                {/* <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+                </Carousel.Caption> */}
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -58,12 +39,11 @@ const Slideshow = () => {
                     src={imageOne}
                     alt="Third slide"
                 />
-
-                <Carousel.Caption>
+                {/* <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item> */}
+                </Carousel.Caption> */}
+            </Carousel.Item>
         </Carousel>
     )
 }
