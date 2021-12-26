3b320f1708144f3865f5f1788ed4729843a03086
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-26 17:20:36 -0500

    starting on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,45 +1,41 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Importing custom CSS
 import './footer.css';
 import '../../fonts/fonts.css';
 
-// importing navdata
-import navData from '../../utils/navdata.json';
-const navLink = navData.links;
-
 export default function Footer() {
 
-    const Links = () => {
+    const SectionOne = () => {
         return (
-            <Nav className="me-auto">
-                {navLink.map((page, index) =>
-                    page.sub ? (
-                        <NavDropdown  title={page.name} id="collasible-nav-dropdown" key={`dropdown-${index}`}>
-                            {page.sub.map((sub, i) => <NavDropdown.Item  href={sub.link} key={`sub-${i}`}>{sub.name}</NavDropdown.Item>)}
-                        </NavDropdown>
-                    ) : (
-                        <Nav.Link href={page.link} key={`page-${index}`}>{page.name}</Nav.Link>
-                    )
-                )}
-            </Nav>
+            <div>
+                <p>
+                    Kings Highway Deliverance Church
+                    500 East Ridge Road
+                    Gary, IN 46409
+                </p>
+            </div>
+        );
+    };
+
+    const SectionTwo = () => {
+        return (
+            <div>
+                <p>Social Media Links</p>
+                <p>Follow West Angeles on Social Media</p>
+            </div>
         );
     }
 
     return (
         <div className="footer position-static" expand="lg" variant="dark">
             <Container>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav>
-                        <Links />
-                    </Nav>
-                </Navbar.Collapse>
+                <div className="column_container">
+                    <SectionOne />
+                    <SectionTwo />
+                </div>
             </Container>
         </div>
     );
