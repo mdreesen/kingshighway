1a9facb6d5417b433e5bc7d354f66a3da9f54ff1
Michael Dreesen <michaeldreesen90@gmail.com>
2021-12-26 21:33:55 -0500

    adding footer content and social media links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Importing custom CSS
 import './footer.css';
 import '../../fonts/fonts.css';
 
+// import social media images
+import facebook from '../../utils/socialMedia/facebook.webp';
+import youtube from '../../utils/socialMedia/youtube.webp';
+import instagram from '../../utils/socialMedia/instagram.webp';
+
 export default function Footer() {
+
+    const mediaArr = [
+        {
+            icon: facebook,
+            link: 'https://m.facebook.com/profile.php?id=158878407477562'
+        },
+        {
+            icon: youtube,
+            link: 'https://www.youtube.com/channel/UC9M4fsfuROt3ZI4cO9SCCjg'
+        },
+        {
+            icon: instagram,
+            link: 'https://www.instagram.com/explore/locations/106359952/kings-highway-deliverance-church-international/'
+        }]
+
+    const Social = () => {
+         return mediaArr.map((each) => {
+             console.log(each.icon)
+            return (
+                <a href={each.link}>
+                    <img className="icon" src={each.icon}></img>
+                </a>
+            );
+        })
+    }
+
+
 
     const SectionOne = () => {
         return (
-            <div>
+            <div className="footer_section">
                 <p>
-                    Kings Highway Deliverance Church
-                    500 East Ridge Road
+                    Kings Highway Deliverance Church<br />
+                    500 East Ridge Road<br />
                     Gary, IN 46409
                 </p>
             </div>
@@ -22,21 +52,32 @@ export default function Footer() {
 
     const SectionTwo = () => {
         return (
-            <div>
-                <p>Social Media Links</p>
-                <p>Follow West Angeles on Social Media</p>
+            <div className="footer_section">
+                <div className="socialMedia">
+                    <div className="social_container">
+                        <Social />
+                    </div>
+                </div>
+                <p>Follow KHDCI on Social Media</p>
+            </div>
+        );
+    }
+
+    const SectionThree = () => {
+        return (
+            <div className="footer_section">
+                <p>Services Times<br />Sunday 10:00am</p>
             </div>
         );
     }
 
     return (
         <div className="footer position-static" expand="lg" variant="dark">
-            <Container>
-                <div className="column_container">
-                    <SectionOne />
-                    <SectionTwo />
-                </div>
-            </Container>
+            <div className="column_container">
+                <SectionOne />
+                <SectionTwo />
+                <SectionThree />
+            </div>
         </div>
     );
 }
