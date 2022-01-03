e7e8c538c470143acf7444e16e4d3c4e98866984
Michael Dreesen <michaeldreesen90@gmail.com>
2022-01-02 22:06:52 -0500

    putting in church data and link

--- src/pages/About/Church/Gary/index.js
+++ src/pages/About/Church/Gary/index.js
@@ -15,9 +15,15 @@ const Gary = () => {
                 </Parallax>
             </section>
             <section>
-                <Link to="/churches">Back</Link>
-                <Link to="/churches/lansing">Lansing</Link>
-                <div>This section will be about KHDCI Gary</div>
+                <h4>Affiliated Churches</h4>
+                <div className='church_links'>
+                    <Link className='link' to="/churches">Back</Link>
+                    <Link className='link' to="/churches/lansing">Lansing</Link>
+                </div>
+                <h4>About Church KHDCI Gary</h4>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In ornare quam viverra orci sagittis eu volutpat. Duis ultricies lacus sed turpis tincidunt. Sed augue lacus viverra vitae congue eu consequat. Amet nisl suscipit adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Iaculis urna id volutpat lacus. Tempus iaculis urna id volutpat. Libero justo laoreet sit amet cursus sit amet dictum. Eget lorem dolor sed viverra ipsum nunc. Et netus et malesuada fames ac turpis egestas integer eget. Elit ut aliquam purus sit amet luctus venenatis lectus. Id faucibus nisl tincidunt eget nullam non. Facilisis mauris sit amet massa vitae. Scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis. Mauris in aliquam sem fringilla. At auctor urna nunc id cursus metus aliquam eleifend mi. Lorem dolor sed viverra ipsum nunc. Convallis convallis tellus id interdum velit laoreet id. Sem integer vitae justo eget.
+
+                    Quam elementum pulvinar etiam non quam lacus. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Elementum integer enim neque volutpat ac tincidunt. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Egestas purus viverra accumsan in nisl nisi scelerisque eu. Justo laoreet sit amet cursus sit amet dictum sit amet. Leo in vitae turpis massa sed elementum tempus. A erat nam at lectus urna duis convallis. Amet porttitor eget dolor morbi non arcu risus quis. Tristique magna sit amet purus gravida quis blandit. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae.</div>
             </section>
         </div>
     );
--- src/pages/About/churches.js
+++ src/pages/About/churches.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Parallax } from 'react-parallax';
 import { Link } from 'react-router-dom'
+import './about.css';
 
 // import images
 import church_gary from '../../utils/churchImages/church_gary.webp';
@@ -23,11 +24,14 @@ const Churches = () => {
                 </Parallax>
             </section>
             <section>
-                <div>This section will be about KHDCI Lansing</div>
-                <div>
-                    <Link to="/churches/gary">Gary</Link>
-                    <Link to="/churches/lansing">Lansing</Link>
+                <h4>Affiliated Churches</h4>
+                <div className='church_links'>
+                    <Link className='link' to="/churches/gary">Gary</Link>
+                    <Link className='link' to="/churches/lansing">Lansing</Link>
                 </div>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In ornare quam viverra orci sagittis eu volutpat. Duis ultricies lacus sed turpis tincidunt. Sed augue lacus viverra vitae congue eu consequat. Amet nisl suscipit adipiscing bibendum est ultricies integer. Enim diam vulputate ut pharetra sit amet. Iaculis urna id volutpat lacus. Tempus iaculis urna id volutpat. Libero justo laoreet sit amet cursus sit amet dictum. Eget lorem dolor sed viverra ipsum nunc. Et netus et malesuada fames ac turpis egestas integer eget. Elit ut aliquam purus sit amet luctus venenatis lectus. Id faucibus nisl tincidunt eget nullam non. Facilisis mauris sit amet massa vitae. Scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis. Mauris in aliquam sem fringilla. At auctor urna nunc id cursus metus aliquam eleifend mi. Lorem dolor sed viverra ipsum nunc. Convallis convallis tellus id interdum velit laoreet id. Sem integer vitae justo eget.
+
+                    Quam elementum pulvinar etiam non quam lacus. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Elementum integer enim neque volutpat ac tincidunt. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Egestas purus viverra accumsan in nisl nisi scelerisque eu. Justo laoreet sit amet cursus sit amet dictum sit amet. Leo in vitae turpis massa sed elementum tempus. A erat nam at lectus urna duis convallis. Amet porttitor eget dolor morbi non arcu risus quis. Tristique magna sit amet purus gravida quis blandit. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae.</div>
             </section>
         </div>
     );
